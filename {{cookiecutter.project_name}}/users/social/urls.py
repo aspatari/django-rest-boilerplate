a48b51532220c16713fefda5bca4d756ec74571c
Artur Spatari <aspatari@starnet.md>
2018-03-22 10:17:43 +0200

    Fix folder structure

--- {{cookiecutter.project_name}}/users/social/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('facebook', views.FacebookLoginView.as_view()),
-    path('google', views.GoogleLoginView.as_view()),
-    path('vk', views.VKLoginView.as_view()),
-]
