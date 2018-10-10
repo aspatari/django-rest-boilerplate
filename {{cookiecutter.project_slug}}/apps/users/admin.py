7a5cf8e09dc1a67c884e818043434b2f8597cb61
Gaponcic Oleg <ogaponcic@starnet.md>
2018-10-10 15:51:44 +0300

    Update django admin user forms

--- {{cookiecutter.project_slug}}/apps/users/admin.py
+++ {{cookiecutter.project_slug}}/apps/users/admin.py
@@ -1,22 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 from . import models
 
 
-# Register your models here.
+@admin.register(models.User)
+class UserAdmin(UserAdmin):
+    fieldsets = (
+        (_("Personal info"), {"fields": ("username", "email", "password")}),
+        (
+            _("Permissions"),
+            {"fields": ("is_active", "is_confirmed", "is_staff", "is_superuser", "groups", "user_permissions")},
+        ),
+        (_("Important dates"), {"fields": ("last_login", "created_at", "updated_at")}),
+    )
+    readonly_fields = ("last_login", "created_at", "updated_at")
+    list_display = ("username", "email", "last_login", "is_active", "is_confirmed", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+    list_filter = ("is_active", "is_confirmed", "is_staff", "is_superuser")
+    ordering = ("username", "email")
 
 
-@admin.register(models.User, models.Profile, models.Transaction)
-class Admin(admin.ModelAdmin):
-    '''
-        Admin View for
-    '''
-    # list_display = ('',)
-    # list_filter = ('',)
-    # inlines = [
-    #     Inline,
-    # ]
-    # raw_id_fields = ('',)
-    # readonly_fields = ('',)
-    # search_fields = ('',)
-
-# admin.site.register(, Admin)
+@admin.register(models.Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related("user")
