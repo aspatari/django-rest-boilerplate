eeb7d654f4af24d8ae1e5658254e1d83794b0200
Gaponcic Oleg <ogaponcic@starnet.md>
2018-10-10 16:18:45 +0300

    Use templated mail for actions

--- {{cookiecutter.project_slug}}/apps/users/actions.py
+++ {{cookiecutter.project_slug}}/apps/users/actions.py
@@ -1,55 +1,63 @@
-from django.core.mail import send_mail
-
+from rest_framework.exceptions import MethodNotAllowed
+from django.conf import settings
+from templated_email import send_templated_mail
 from apps.users import serializers
 from . import utils
 
 
 class PostRegistrationConfirmAction(utils.GenericAction):
-    name = 'Post Registration Confirm email Action'
-    description = 'Action for sending confirmation email after user register and confirm after access'
+    name = "Post Registration Confirm email Action"
+    description = "Action for sending confirmation email after user register and confirm after access"
 
     def apply(self):
         user = self.get_object()
         user.is_confirmed = True
-        user.save(update_fields=['is_confirmed'])
+        user.save(update_fields=["is_confirmed"])
+        self.obj.delete()
 
     def post_create(self):
-        # TODO Add tempalted mail and use celery
-        send_mail(
-            subject="Confirmation Email",
-            message=f'{self.obj.id}',
-            from_email='support@bookmarks.com',
-            recipient_list=[f'{self.obj.user.email}']
+        host = getattr(settings, "CLIENT_HOST")
+        url = f"{host}/user/confirm/{self.obj.id}"
+        send_templated_mail(
+            template_name="email-confirm-user",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[self.obj.user.email],
+            context={"url": url},
         )
 
 
 class ResetPasswordAction(utils.GenericAction):
-    name = 'User reset password'
-    description = 'Action for resenting user password'
+    name = "User reset password"
+    description = "Action for resenting user password"
     serializer_class = serializers.ResetUserPasswordSerializer
 
     def apply(self):
         # Extract data
         user = self.get_object()
-        request = self.settings.get('request')
+        request = self.settings.get("request")
 
         # Validate input data
+        if not request:
+            raise MethodNotAllowed(method="GET")
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        password = serializer.validated_data.get('password')
+        password = serializer.validated_data.get("password")
 
         # Set data
         user.set_password(password)
-        user.save(update_fields=['password'])
+        user.save(update_fields=["password"])
+        self.obj.delete()
 
     def post_create(self):
-        # TODO Add tempalted mail and use celery
-        send_mail(
-            subject="Confirmation Email",
-            message=f'{self.obj.id}',
-            from_email='support@bookmarks.com',
-            recipient_list=[f'{self.obj.user.email}']
+        host = getattr(settings, "CLIENT_HOST")
+        url = f"{host}/login/reset/{self.obj.id}"
+
+        send_templated_mail(
+            template_name="email-reset-password",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[self.obj.user.email],
+            context={"url": url},
         )
 
     def get_serializer(self, *args, **kwargs):
