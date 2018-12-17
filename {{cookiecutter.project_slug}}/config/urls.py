cab9a446e25dc2b2773c521a35612130f94105db
Artur Spatari <artemir0106@gmail.com>
2018-12-17 09:37:09 +0200

    Update urls.py

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -20,22 +20,22 @@ from django.contrib import admin
 from django.urls import path
 from django.views import defaults as default_views
 from drf_yasg import openapi
-# from drf_yasg.views import get_schema_view
+from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Snippets API",
-#         default_version='v1',
-#         description="Test description",
-#         terms_of_service="https://www.google.com/policies/terms/",
-#         contact=openapi.Contact(email="artemir0106@gmail.com"),
-#         license=openapi.License(name="BSD License"),
-#     ),
-#     # validators=['flex', 'ssv'],
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-# )
+schema_view = get_schema_view(
+     openapi.Info(
+         title="{{{{cookiecutter.project_name}}}}",
+         default_version='v1',
+         description="Test description",
+         terms_of_service="https://www.google.com/policies/terms/",
+         contact=openapi.Contact(email="artemir0106@gmail.com"),
+         license=openapi.License(name="BSD License"),
+     ),
+     # validators=['flex', 'ssv'],
+     public=True,
+     permission_classes=(permissions.AllowAny,),
+ )
 
 urlpatterns = [
                   path(settings.ADMIN_URL, admin.site.urls),
@@ -44,7 +44,7 @@ urlpatterns = [
 
                   # url(r'^swagger(?P<format>.json|.yaml)$', schema_view.without_ui(cache_timeout=None),
                   #     name='schema-json'),
-                  # url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
+                  path('', schema_view.with_ui('swagger', cache_timeout=None), name='schema-swagger-ui'),
                   # url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=None), name='schema-redoc'),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
